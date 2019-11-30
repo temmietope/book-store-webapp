3bee988bdf8401d8753aa29e0aa5a9de0fc48508
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-11-30 07:38:32 +0100

    created middleware for authentication

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
@@ -10,7 +11,7 @@ const User = require("../models/User");
 // @ route  GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get("/", (req, res) => {
+router.get("/", auth, (req, res) => {
   res.send("Get logged in user");
 });
 
