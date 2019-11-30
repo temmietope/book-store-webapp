21f7d11169ac29fd35c88fd99e1a69c54f0efe6e
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-11-30 07:45:06 +0100

    get logged in user

--- routes/auth.js
+++ routes/auth.js
@@ -11,8 +11,14 @@ const User = require("../models/User");
 // @ route  GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get("/", auth, (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 // @ route  POST api/auth
