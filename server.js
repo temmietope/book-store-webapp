93e1ef633129eaae50b70aa98fb9fcde7054b9a6
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-11-30 07:27:54 +0100

    generate token

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome you your awesome bookstore" })
 );
