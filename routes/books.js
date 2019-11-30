bac0ccf572b3ec7fc4e0b758447729970a2d9b88
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-11-30 08:02:19 +0100

    get all user's books

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Book = require("../models/Book");
 
 // @ route  GET api/books
 // @desc    Get all users books
 // @access  Private
-router.get("/", (req, res) => {
-  res.send("Get all books");
+router.get("/", auth, async (req, res) => {
+  try {
+    const books = await Book.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(books);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @ route  POST api/books
