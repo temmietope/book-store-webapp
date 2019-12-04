fc2362e9607a016bcc9178b4989b5ca21ca25d2e
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-12-05 00:30:40 +0100

    authContext

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,25 @@ import Navbar from "./Components/layouts/Navbar/index";
 import Home from "./Components/pages/Home";
 import About from "./Components/pages/About";
 import BookState from "./Context/book/BookState";
+import AuthState from "./Context/auth/AuthState";
 
 const App = () => {
   return (
-    <BookState>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
+    <AuthState>
+      <BookState>
+        <Router>
+          <div className="app">
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
-    </BookState>
+        </Router>
+      </BookState>
+    </AuthState>
   );
 };
 
