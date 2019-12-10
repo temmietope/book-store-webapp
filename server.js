bdab8cbc1852a65eaf474c992a24064d4e7f8ccb
Temitope Ayodele <39211865+temmietope@users.noreply.github.com>
2019-12-10 11:27:20 +0100

    removed initial route

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Welcome you your awesome bookstore" })
-);
-
 //Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
